1528dd8b9054bc729d03bf7712e543fb402acec7
YuvanX <abhivigneshofficial@gmail.com>
2025-02-07 14:51:02 +0530

    fixed image issuses

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { InteractiveHoverButton } from "./interactive-hover-button";
 import { ShineBorder } from "./shine-border";
 import Tech from "./techCard";
+import Image from "next/image";
 
 export default function Card({
   icon,
@@ -21,10 +22,10 @@ export default function Card({
   return (
     <ShineBorder color={["#A07CFE", "#FE8FB5", "#FFBE7B"]} className="m-5 lg:m-10">
       <div className="flex flex-col p-3 cursor-pointer">
-        <div className="my-4"><img src={icon} className="w-10 h-10 rounded-full"/></div>
+        <div className="my-4"><Image width={10} height={10} alt="icon" src={icon} className="w-10 h-10 rounded-full"/></div>
         <div className="font-custom font-semibold text-xl mb-2">{title}</div>
         <div className="mb-2 text-gray-400 line-clamp-3">{description}</div>
-        <div className="mt-2 mb-4 flex flex-wrap gap-2">{tech.map(t => <Tech tech={t}/>)}</div>
+        <div className="mt-2 mb-4 flex flex-wrap gap-2">{tech.map(t => <Tech key={t} tech={t}/>)}</div>
         <div className="flex gap-2 items-center">
           <div>
             <Link href={websiteLink}>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <div className="text-center mx-10">
       <div>
         <div className="mx-2 mt-44 mb-10 flex justify-center">
-            <img className="w-20 h-20 rounded-full" src="/JOIN.png"/>
+            <Image width={20} height={20} alt="icon" className="w-20 h-20 rounded-full" src="/JOIN.png"/>
         </div>
-        <h1 className="text-5xl font-bold font-custom">Hi, I'm Abhi Vignesh <span className="text-xs text-purple-500 font-bold">a.k.a yuvan</span></h1>
+        <h1 className="text-5xl font-bold font-custom">Hi, I&apos;m Abhi Vignesh <span className="text-xs text-purple-500 font-bold">a.k.a yuvan</span></h1>
         <p className="mx-2 my-6 text-lg font-thin">
         Full Stack Web Developer building scalable and high-performance web applications. I specialize in both frontend and backend development, creating seamless user experiences. Passionate about solving real-world challenges through clean and efficient code.
         </p>
